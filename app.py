9efd6f10ba9ba1b10fee0bec447a899fac073374
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-07 09:48:09 -0300

    Cadastrando mais livros

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 livros = [
     {
         'id': 1,
-        'titulo': 'O senhor dos anéis - A sociedade do anel'
+        'titulo': 'O senhor dos anéis - A sociedade do anel',
+        'autor': 'J.R.R Tokien' 
+    },
+       
+    {
+         'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling' 
+    },
+
+    {
+         'id': 3,
+        'titulo': 'Hábitos Atômicos',
+        'autor': 'James Clear' 
     }
+
 ]
