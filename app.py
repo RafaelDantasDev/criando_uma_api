271f54b25fe29459c2531140bc86c4494ee127be
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-07 10:36:48 -0300

    Adicionado a função editar da API

--- app.py
+++ app.py
@@ -26,13 +26,27 @@ livros = [
 ]
 
 # Consultar(Todos)
-@app.route('/livros', methods=[GET])
+@app.route('/livros', methods=['GET'])
 def obter_livros():
     return jsonify(livros)
 
 # Consultar por id
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
 
 # Editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+# Criar
 
 # Excluir
 
